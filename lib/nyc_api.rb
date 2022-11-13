7a032ff27214df0ab052fc51518fbd85b6d29120
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-11-14 01:38:19 +0300

    completed assignment

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'open-uri'
 require 'json'
+require 'awesome_print'
  
 class GetPrograms
 
@@ -9,10 +10,17 @@ class GetPrograms
   def get_programs
     uri = URI.parse(URL)
     response = Net::HTTP.get_response(uri)
-    response.body
+     response.body
+    # JSON.parse(response.body)
+  end
+  def program_school
+    programs = JSON.parse(self.get_programs)
+    programs.collect {|program| program['agency']}
   end
 
 end
 
-programs = GetPrograms.new.get_programs
-puts programs
+# programs = GetPrograms.new.get_programs
+# puts programs
+programs = GetPrograms.new
+puts programs.program_school.uniq
